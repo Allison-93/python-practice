1b129ea3bdf423b157bb4350dee0d3ebca3143f9
Allison-93 <allisonmoopanar@gmail.com>
2025-06-23 19:44:40 +0200

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,17 @@
+fruits = ["apple", "banana", "cherry", "kiwi"]
+print(fruits[0]) 
+
+fruits[1] = "blueberry" 
+print(fruits) 
+
+fruits.append("kiwi")
+print(fruits)
+
+fruits.insert(1, "orange")
+print(fruits) 
+
+fruits.remove("kiwi")
+print(fruits)
+
+fruits.sort(reverse=True)
+print(fruits)
