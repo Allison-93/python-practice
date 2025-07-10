436bd26c4360b7120990bab8e3847086cbc574e3
Allison-93 <allisonmoopanar@gmail.com>
2025-07-10 17:29:51 +0200

    Create exception_handling.py

---
+++ exception_handling.py
@@ -0,0 +1,11 @@
+try: 
+    print(x)
+  
+except:
+    print("Variable x is not defined")
+else:
+    print("Everything went wrong")    
+    
+    x = -1        
+    if x < 0: 
+        raise Exception("sorry, no numbers below zero")
